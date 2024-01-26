b2a9d3a266512866e532a70b62417ac9faa4cdda
oktalaia <oktalaia23@smk.belajar.id>
2024-01-26 09:17:25 +0700

    user CRUD

---
+++ home.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php include "menu.php" ?>
+</body>
+</html>
---
+++ update-user.php
@@ -0,0 +1,31 @@
+<?php 
+
+require "koneksi.php";
+
+session_start();
+
+if ($_POST["id"] == $_SESSION["id"]) {
+    echo "Tidak bisa edit user yang sedang aktif";
+    exit;
+}
+
+$id = $_POST["id"];
+$username = $_POST["username"];
+$level = $_POST["level"];
+
+$pasword = $_POST["old_password"];
+
+$new_password = trim($_POST["password"]);
+if (strlen($new_password) > 0) {
+
+    $password = password_hash($new_password, PASSWORD_DEFAULT);
+}
+
+$sql = "UPDATE user SET username = '$username', password = '$password', level = '$level' WHERE id = '$id'";
+mysqli_query($koneksi, $sql);
+
+if (mysqli_error($koneksi)) {
+    echo mysqli_error($koneksi);
+} else {
+    header ("location: user.php");
+}
