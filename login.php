5ad3ddd45bed9f3f19e076f524b8fddfc18da6e1
oktalaia <oktalaia23@smk.belajar.id>
2024-02-23 14:38:15 +0700

    perbaiki halaman login

--- login.php
+++ login.php
@@ -3,26 +3,41 @@
 <head>
     <title>Halaman Login</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <script>
+        function hideIcon(input) {
+            const icon = input.previousElementSibling;
+            if (input.value.length > 0) {
+                icon.classList.add('hidden');
+            } else {
+                icon.classList.remove('hidden');
+            }
+        }
+    </script>
 </head>
 <body>
-    <form action="validasi.php" method="POST">
+    <div class="login-container">
         <h1>Selamat Datang!</h1>
-        <table>
-            <tr>
-                <td>Username</td>
-                <td><input type="text" name="username"></td>
-            </tr>
-            <tr>
-                <td>Password</td>
-                <td><input type="password" name="password"></td>
-            </tr>
-            <tr>
-                <td colspan="2">
-                    <button type="submit">LOGIN</button>
-                    <button type="reset">CLEAR</button>
-                </td>
-            </tr>
-        </table>
-    </form>
+        <form action="validasi.php" method="POST">
+            <div class="input-container">
+                <label for="username">Username</label>
+                <div class="icon-input">
+                    <i class="fas fa-user"></i>
+                    <input type="text" name="username" id="username" onfocus="hideIcon(this)" onblur="hideIcon(this)" required>
+                </div>
+            </div>
+            <div class="input-container">
+                <label for="password">Password</label>
+                <div class="icon-input">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" name="password" id="password" onfocus="hideIcon(this)" onblur="hideIcon(this)" required>
+                </div>
+            </div>
+            <div class="button-container">
+                <button type="submit">LOGIN</button>
+                <button type="reset">CLEAR</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
